e8fa108f89a12f15f5b1e5c09062cb07140c9d4c
IlariaAngeloni <angeloniilaria@yahoo.it>
2020-11-05 16:36:52 +0100

    Add files via upload

---
+++ path.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main (int argc, char *argv[])
+{
+char buff[1024];
+if( NULL!=getcwd(buff,sizeof(buff)) )
+	puts(buff);
+else
+	perror("getcwd() error");
+return 0;
+}
+
